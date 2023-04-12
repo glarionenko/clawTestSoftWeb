524e4342bf014a28acf6d3ea814d2a7c54bb7cd5
Georgii Larionenko <41651290+glarionenko@users.noreply.github.com>
2023-04-12 20:28:58 +0200

    intervals for endswitches fix

--- js/mainCode.js
+++ js/mainCode.js
@@ -8,8 +8,8 @@ function sendMessageToMQTTT(topicName,msgText){
         console.log("Can't send");
     }
 }
-var intervalIDs =[0,0,0,0,0,0,0,0,0,0,0,0];
-var elementIDs =["leftEnd","rightEnd","upEnd","downEnd","forwardEnd","backwardEnd"];
+
+var currentIntervalId ={"leftEnd":0,"rightEnd":0,"upEnd":0,"downEnd":0,"forwardEnd":0,"backwardEnd":0};
 function setEndValue(id, value) {
     var badge = document.getElementById(id);
     if (value == 1) {
@@ -21,20 +21,23 @@ function setEndValue(id, value) {
     }
   
     var lastUpdate = new Date().getTime(); // время последнего обновления
-  
+    var lastIntervalID = currentIntervalId.get(id);
+    if(lastIntervalID!==0){
+      clearInterval(lastIntervalID);
+    }
     var myIntervalID = setInterval(function() {
       var now = new Date().getTime(); // текущее время
       var diff = now - lastUpdate; // разница в миллисекундах
       if (diff >= 60000) { // если прошло более минуты, останавливаем обновление
-        //badge.innerText = ">1 min";
-        var seconds = Math.floor((diff % 60000) / 1000); // вычисляем количество секунд
-        badge.innerText = seconds + " sec";
+        badge.innerText = ">1 min";
         clearInterval(myIntervalID);
+        currentIntervalId.set(id,0);
       } else { // обновляем содержимое элемента
         var seconds = Math.floor((diff % 60000) / 1000); // вычисляем количество секунд
         badge.innerText = seconds + " sec";
       }
     }, 1000); // обновляем каждую секунду
+    currentIntervalId.set(id,myIntervalID);
   
   }
 $(document).ready(function () {
